850f85c56871c88de378675b4bf405e5968bae7c
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-08-01 00:01:32 -0400

    finished sprint

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [character, setCharacter] = useState();
+  
 
+  const characters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  useEffect(() => {
+    characters.map(index => {
+      axios.get(`https://swapi.dev/api/people/${index}`)
+      .then(response => { console.log(response)
+        setCharacter(response.data);
+        
+      })
+      .catch(error => console.log(error));
+    })
+    
+  } , []);
+  
+  console.log(character);
+  console.log(characters);
+  
+  if(!character) return <h1>Loading...</h1>
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Inhabitants Of A Galaxy Far, Far Away</h1>
+      
+       {characters.map(() => {
+        return <Character character={character}/>
+       })}
+      
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,47 @@
 // Write your Character component here
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+
+const C1 = styled.div`
+    background-color: rgba(128,128,128,0.5);
+    width: 50%;
+    margin-left: 25%;
+    border: 1px solid black;
+    border-radius: 30%;
+    margin: 30px auto 30px auto;
+`;
+
+const Name = styled.h2`
+    font-size: 2em;
+    font-style: italic;
+    text-decoration: underline;
+`;
+
+const Date = styled.h3`
+    font-size: 1.3em;
+`;
+
+const Misc = styled.h4`
+    font-size: 1.1em;
+    font-style: italic;
+
+`;
+const Character = props => {
+    return (
+        <div>
+                <C1 className="character1">
+                    <Name>{props.character.name}</Name>
+                    <Date> Birth Year: {props.character.birth_year}</Date>
+                    <Misc>Height: {props.character.height}</Misc>
+                    <Misc>Hair Color: {props.character.hair_color}</Misc>
+                    <Misc>Skin Color: {props.character.skin_color}</Misc>
+                </C1>
+        </div>
+    )
+
+
+}
+
+export default Character;
